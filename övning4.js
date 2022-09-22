2bb38b53235e9dbf9414e9c05c8e33f728d0104f
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-22 20:53:56 +0200

    Update övning4.js

--- övning4.js
+++ övning4.js
@@ -1,4 +1,27 @@
 /*
 Palindrome
 Programmet tar in ett tal och kollar om det är en palindrom eller inte.
+notes för mig själv -->
+dvs ett tal som man kan skriva baklängs och det kommer vara det samma
+ex 121, 44, 545 etc
 */
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let rest = 0;
+let summary = 0;
+let number = parseInt(prompt(">"));
+let temporaryNumber = number;
+
+while(number>0){
+    rest = number%10;
+    number = parseInt(number/10);
+    summary = (summary*10)+rest;
+};
+if(summary==temporaryNumber){
+    console.log("Palindrome");
+}
+else{
+    console.log("Not palindrome");
+};
