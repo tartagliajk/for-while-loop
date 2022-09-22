99266cc19b3a017efe25eb4783ef86d5514a04cb
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-22 17:09:30 +0200

    övning 1

---
+++ for-while-struktur.js
@@ -0,0 +1,15 @@
+/*For loops
+for ([initialization]; [condition]; [final-expression];){}*/
+let array = [];
+for (var i = 0; i > 5; i++){
+    array.push(i);
+}
+
+/*while loops
+while(condition){} */
+
+let i = 0;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
--- övning1.js
+++ övning1.js
@@ -2,3 +2,12 @@
 Summa
 Programmet tar in ett heltal och beräknar summan av alla tal från 1 till det givna talet
 */
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let number = parseInt(prompt("Write your number > "));
+let sum = 0;
+for (let i = 1; i <= number; i++){
+    sum=sum + i
+};
+console.log(sum);
