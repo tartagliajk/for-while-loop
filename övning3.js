4ebc1111211f401c35ce9aedd26f4a6b24df68a3
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-22 20:07:46 +0200

    Update övning3.js

--- övning3.js
+++ övning3.js
@@ -9,3 +9,17 @@ Output:
 2 1
 1
 */
+
+var ps = require("prompt-sync");
+var prompt = ps();
+let rows = parseInt(prompt("Write your number > "));
+while(rows >= 1){
+    let j = parseInt(rows);
+    while(j >= 1){
+        //tydligen kan den inte skriva ut endast variabler utan behöver en sträng innan
+        process.stdout.write("" + j);
+        j--;
+    }
+    rows--;
+    process.stdout.write("\n");
+}
