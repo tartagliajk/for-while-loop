60413257a62610145d2af9e98af6db47b4c3e788
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-22 19:36:48 +0200

    Update övning2.js

--- övning2.js
+++ övning2.js
@@ -10,3 +10,21 @@ Output:
 #####
 #####
 */
+
+var ps = require("prompt-sync");
+var prompt = ps();
+let number=parseInt(prompt("Write your number > "))
+//ändrar antal columns beronde på startnummer
+let columns =1;
+while(columns<=number)
+{
+    let row=1;
+    while(row<=number)
+    {
+        //gör så att man kan skriva på samma line
+        process.stdout.write("#");
+        row++;
+    }
+    process.stdout.write("\n");
+    columns++;
+}
